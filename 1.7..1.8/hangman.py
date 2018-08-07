5ad90692630830fbb9a75c90d1d1d9dba4441d2e
onhan <artem.shmahun@gmail.com>
2018-08-07 16:42:32 +0300

    1.7..1.8: displays the message "Wrong letters"

--- 1.7..1.8/hangman.py
+++ 1.7..1.8/hangman.py
@@ -37,6 +37,16 @@ HANGMAN_PICS = ['''
 	     ===''']
 words = 'аист акула бабуин баран барсук бобр бык верблюд волк воробей ворон выдра голубь гусь жаба зебра змея индюк кит кобра коза койот корова кошка кролик крыса курица лама ласка лебедь лев лиса лосось лось лягушка медведь моллюск моль мул муравей мышь норка носорог обезьяна овца окунь олен орел осел панда паук питон попугай пума семга скунс собака сова тигр тритон тюлень утка форель хорек черепаха ястреб ящерица'.split()
 
+
 def getRandomWorld(wordList):
 	wordIndex = random.randint(0, len(wordList) - 1)
 	return wordList[wordIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+	print(HANGMAN_PICS[len(missedLetters)])
+	print()
+
+	print('Ошибочные буквы:', end=' ')
+	for letter in missedLetters:
+		print(letter, end=' ')
+	print()
